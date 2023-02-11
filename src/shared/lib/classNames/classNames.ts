d9537a67e6a8d59ec173fb95fc8251cff249c23f
Alexander Vetchinkin <ASVetchinkin@sberbank.ru>
2023-02-11 19:06:39 +0300

    [2.12]: add Navbar widget

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,16 +2,14 @@ type Mods = Record<string, string | boolean>
 
 export function classNames(
     cls: string,
-    mods: Mods,
-    additional: string[]
+    mods: Mods = {},
+    additional: string[] = []
 ): string {
     return [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         Object.entries(mods)
             .filter(([, value]) => Boolean(value))
             .map(([cls]) => cls),
     ].join(' ')
 }
-
-classNames('remove-btn', { hover: true, selectebel: true, red: false }, ['pdg'])
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+
+import cls from './Navbar.module.sass'
+
+interface NavbarProps {
+    className?: string
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={'/'}
+                    className={cls.mainLink}
+                >
+                    Main
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                    About webpage
+                </AppLink>
+            </div>
+        </div>
+    )
+}
