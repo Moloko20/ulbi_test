cc4a6edbf1abd5af93db0c72cdaacc804d457bc7
Alexander Vetchinkin <ASVetchinkin@sberbank.ru>
2023-02-11 14:36:19 +0300

    [2.11]: restrusture app routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
-import { Suspense } from 'react'
 import { Link } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
 
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
 import { useTheme } from './providers/ThemeProvider'
 import { classNames } from 'shared/lib/classNames/classNames'
 
 import './styles/index.sass'
+import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme()
@@ -16,15 +14,9 @@ export const App = () => {
         <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Сменить тему</button>
 
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About webpage</Link>
+            <Navbar />
 
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage />} />
-                    <Route path="/" element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     )
 }
